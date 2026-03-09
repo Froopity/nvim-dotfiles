2587a2ac3a68bb667b5c301bdf8c66e3dfffee21
Will Bailey <william.bailey@onyxtech.com.au>
2026-03-09 17:07:53 +0800

    Minor oil config

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,7 +5,12 @@ return {
   opts = {
     view_options = {
       show_hidden = true,
-    }
+    },
+    columns = {
+      "icons",
+      "mtime",
+    },
+    skip_confirm_for_simple_edits = true,
   },
   keys = {
     { "<leader>o", "<CMD>Oil<CR>", desc = "Open Oil" },
