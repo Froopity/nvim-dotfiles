6ac43c2653d813ce7e426dc5b653a532d64d4438
Will Bailey <william.bailey@onyxtech.com.au>
2026-03-10 14:15:05 +0800

    Switch to minisurround for no real reason

---
+++ lua/plugins/mini-surround.lua
@@ -0,0 +1,18 @@
+return {
+  "nvim-mini/mini.surround",
+  version = false,
+  config = function()
+    require('mini.surround').setup({
+      custom_surroundings = {
+        -- Invert brackets: ( for no space, ) for space
+        ['('] = { output = { left = '(', right = ')' } },
+        [')'] = { output = { left = '( ', right = ' )' } },
+        ['['] = { output = { left = '[', right = ']' } },
+        [']'] = { output = { left = '[ ', right = ' ]' } },
+        ['{'] = { output = { left = '{', right = '}' } },
+        ['}'] = { output = { left = '{ ', right = ' }' } },
+      }
+    })
+    vim.keymap.set('n', '<leader>s', 'saiw', { remap = true, desc = 'Surround word' })
+  end
+}
