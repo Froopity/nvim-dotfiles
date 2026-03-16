a207d1f9e0a7c9cee5de20d774229ef7cb7caf30
Will Bailey <william.bailey@onyxtech.com.au>
2026-03-16 16:17:47 +0800

    Switch to mini-files + some old oil config + snacks config

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -8,6 +8,5 @@ return {
     })
 
     vim.keymap.set('n', '<leader>.', function() snacks.scratch() end, { desc = "Toggle Scratchpad" })
-    vim.keymap.set('n', '<leader>S', function() snacks.scratch.select() end, { desc = "Select Scratchpad" })
   end
 }
