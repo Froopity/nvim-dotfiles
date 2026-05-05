3fd4b8b849872121961bd57b52a74d942e02a0b1
Will Bailey <william.bailey@onyxtech.com.au>
2026-05-05 11:13:22 +0800

    Disable autopairs, modify keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,15 +5,12 @@ vim.keymap.set('i', 'jk', '<Esc>')
 vim.keymap.set('n', '<leader>t', ':terminal<CR>')
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
 
--- Use alt-direction to switch windows, even in insert mode
-vim.keymap.set({ 't', 'i' }, '<A-h>', '<C-\\><C-n><C-w>h')
-vim.keymap.set({ 't', 'i' }, '<A-j>', '<C-\\><C-n><C-w>j')
-vim.keymap.set({ 't', 'i' }, '<A-k>', '<C-\\><C-n><C-w>k')
-vim.keymap.set({ 't', 'i' }, '<A-l>', '<C-\\><C-n><C-w>l')
-vim.keymap.set('n', '<A-h>', '<C-w>h')
-vim.keymap.set('n', '<A-j>', '<C-w>j')
-vim.keymap.set('n', '<A-k>', '<C-w>k')
-vim.keymap.set('n', '<A-l>', '<C-w>l')
+-- Unbind cmd-line window to stop accidental invocation. Use :<C-f> to open.
+vim.keymap.set('n', 'q:', '<nop>')
+
+-- Use alt-j/k to navigate QuickFix menu
+vim.keymap.set('n', '<A-j>', ':cnext<CR>')
+vim.keymap.set('n', '<A-k>', ':cprevious<CR>')
 
 -- Select the last pasted text
 vim.keymap.set('n', 'gp', "'[v`]", { desc = "Select last pasted text" })
@@ -28,9 +25,14 @@ vim.api.nvim_create_autocmd("FileType", { -- Unmap enter when in QuickFix
   end,
 })
 
+-- Copy/Delete entire buffer
+vim.keymap.set('n', ' ya', "mqggyG'q", { desc = "Yank entire buffer" })
+vim.keymap.set('n', ' da', 'gg"_dG', { desc = "Delete entire buffer" })
+
 -- LSP keymaps
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic error under cursor' })
 vim.keymap.set('i', '<C-Space>', function() vim.lsp.completion.get() end, { desc = 'Trigger LSP completion' })
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'Go to definition' })
 
 -- Misc
 vim.keymap.set('n', '<leader><ESC>', ':nohlsearch<CR>', { silent = true, desc = 'Remove search highlights' })
