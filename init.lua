b8707c888162cc114c5c6161bf48aa684790fc4e
Will Bailey <william.bailey@onyxtech.com.au>
2026-05-05 11:12:58 +0800

    Add neovide config

--- init.lua
+++ init.lua
@@ -4,3 +4,7 @@ vim.g.maplocalleader = '\\'
 require("config.options")
 require("config.lazy")
 require("config.keymaps")
+
+if vim.g.neovide then
+  require("config.neovide")
+end
---
+++ lua/config/neovide.lua
@@ -0,0 +1,3 @@
+vim.g.neovide_scale_factor = 0.8
+vim.o.guifont = "FiraCode Nerd Font Mono"
+vim.g.neovide_cursor_vfx_mode = "pixiedust"
