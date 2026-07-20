183c6ce1e8400f5787309c959312130a0b43e37c
Will Bailey <william.bailey@onyxservices.com.au>
2026-07-20 11:13:14 +0800

    Nicer codeblock markdown rendering

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -2,11 +2,9 @@ return {
   'MeanderingProgrammer/render-markdown.nvim',
   ft = { 'markdown' },
   dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },
-  ---@module 'render-markdown'
-  ---@type render.md.UserConfig
   opts = {
     code = {
-      conceal_delimiters = false,
+      border = 'thick',
     },
   },
 }
